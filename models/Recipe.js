66deddde7e03d8a484e4a81dc0dd84190b8f5176
nickarredondo <Nicka721@gmail.com>
2021-07-30 15:50:43 -0700

    added Recipe Schema

---
+++ models/Recipe.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.schema ({
+    title : {
+        type: String, 
+        requires: [true, "Title is needed"]
+    },
+    image: {
+        type: String, 
+        required: [true, "Image is needed"]
+    },
+    category :{
+        type: String,
+        required:[true , "Oops you forgot to put a category" ]
+    },
+    ingredients: {
+        type: [String], 
+        require: [true, "Oops you forgot to put your ingredients!"]
+    },
+    instructions:{
+        type :[String], 
+        require: [true, "Oops you forgot to put your instructions!"]
+    },
+  },
+ {
+    timestamps:true 
+ });
+
+ const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
